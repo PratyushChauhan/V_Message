840cf6d12f2081b38eab52a96b799a6b1d8f114a
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2023-01-02 15:21:30 +0530

    Add spinner to login and register process.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:flash_chat/components/alerts.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   static String id = "login_screen";
@@ -15,87 +16,101 @@ class _LoginScreenState extends State<LoginScreen> {
   String email;
   String password;
   FirebaseAuth _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: "logo",
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: "logo",
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            // SizedBox(
-            //   height: 48.0,
-            // ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black),
-              onChanged: (value) {
-                //TODO Do something with the user input.
-                email = value;
-              },
-              decoration: kEmailInputDecoration,
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black),
-              onChanged: (value) {
-                //TODO Do something with the user input.
-                password = value;
-              },
-              decoration: kPasswordInputDecoration,
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            HeroButton(
-              context,
-              tag: "login",
-              color: Colors.lightBlueAccent,
-              text: "Log In",
-              onPressed: () async {
-                try {
-                  final user = await _auth.signInWithEmailAndPassword(
-                      email: email, password: password);
-                  if (user != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
-                  }
-                } catch (e) {
-                  //check if e is firebase auth exception
-                  if (e is FirebaseAuthException) {
-                    //check if e is user not found
-                    if (e.code == "user-not-found") {
-                      popupError(context,
-                          "User not found.\nCheck email and password.");
+              // SizedBox(
+              //   height: 48.0,
+              // ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black),
+                onChanged: (value) {
+                  //TODO Do something with the user input.
+                  email = value;
+                },
+                decoration: kEmailInputDecoration,
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black),
+                onChanged: (value) {
+                  //TODO Do something with the user input.
+                  password = value;
+                },
+                decoration: kPasswordInputDecoration,
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              HeroButton(
+                context,
+                tag: "login",
+                color: Colors.lightBlueAccent,
+                text: "Log In",
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (user != null) {
+                      setState(() {
+                        showSpinner = false;
+                      });
+                      Navigator.pushNamed(context, ChatScreen.id);
                     }
-                    //check if e is wrong password
-                    else if (e.code == "wrong-password") {
-                      popupError(context, "Wrong password.\nPlease try again.");
+                  } catch (e) {
+                    setState(() {
+                      showSpinner = false;
+                    });
+                    //check if e is firebase auth exception
+                    if (e is FirebaseAuthException) {
+                      //check if e is user not found
+                      if (e.code == "user-not-found") {
+                        popupError(context,
+                            "User not found.\nCheck email and password.");
+                      }
+                      //check if e is wrong password
+                      else if (e.code == "wrong-password") {
+                        popupError(
+                            context, "Wrong password.\nPlease try again.");
+                      } else {
+                        //handle misc errors
+                        popupError(context, "${e.code}\n${e.message}");
+                      }
                     } else {
-                      //handle misc errors
-                      popupError(context, "${e.code}");
+                      popupError(context, "${e.toString()}");
                     }
-                  } else {
-                    popupError(context, "Unknown error.");
                   }
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -4,6 +4,7 @@ import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = "registration_screen";
@@ -15,81 +16,94 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   String email;
   String password;
   final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: "logo",
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: "logo",
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            // SizedBox(
-            //   height: 5,
-            // ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black),
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kEmailInputDecoration,
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kPasswordInputDecoration,
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            HeroButton(
-              context,
-              tag: "register",
-              text: "Register",
-              color: Colors.blueAccent,
-              onPressed: () async {
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(
-                      email: email, password: password);
-                  if (newUser != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
-                  }
-                } catch (e) {
-                  //check if e is a firebase exception
-                  if (e is FirebaseAuthException) {
-                    //check if the error is a weak password
-                    if (e.code == 'weak-password') {
-                      //show a dialog
-                      popupError(context,
-                          "Weak Password.\nMinimum length 6 characters.");
-                    } else if (e.code == 'email-already-in-use') {
-                      popupError(
-                          context, "Email already in use.\nPlease try again.");
+              // SizedBox(
+              //   height: 5,
+              // ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black),
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kEmailInputDecoration,
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kPasswordInputDecoration,
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              HeroButton(
+                context,
+                tag: "register",
+                text: "Register",
+                color: Colors.blueAccent,
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    if (newUser != null) {
+                      setState(() {
+                        showSpinner = false;
+                      });
+                      Navigator.pushNamed(context, ChatScreen.id);
                     }
-                  } else {
-                    popupError(context, "Unknown error.\nPlease try again.");
+                  } catch (e) {
+                    setState(() {
+                      showSpinner = false;
+                    });
+                    //check if e is a firebase exception
+                    if (e is FirebaseAuthException) {
+                      //check if the error is a weak password
+                      if (e.code == 'weak-password') {
+                        //show a dialog
+                        popupError(context,
+                            "Weak Password.\nMinimum length 6 characters.");
+                      } else if (e.code == 'email-already-in-use') {
+                        popupError(context,
+                            "Email already in use.\nPlease try again.");
+                      }
+                    } else {
+                      popupError(context, "Unknown error.\nPlease try again.");
+                    }
                   }
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
